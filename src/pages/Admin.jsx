95381fbd93edbbc60da15c06184f26a509665aa1
eiduduu <eduardo.games098@gmail.com>
2025-12-03 14:48:31 -0300

    resolvendo erro de conflito --forc

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -13,7 +13,7 @@ function Admin() {
     <div className="px-3 mt-4 space-y-4 max-w-md mx-auto">
       <div className="text-sm font-bold text-gray-800">Admin</div>
       <div className="grid grid-cols-2 gap-3">
-        <Link to="/admin/publicacoes" className="p-4 rounded-xl border border-gray-200 bg-white shadow-sm text-center text-xs font-semibold text-gray-800">Publicações</Link>
+        <Link to="/admin/publicacoes" className="p-6 rounded-xl border border-gray-200 bg-white shadow-sm text-center text-xs font-semibold text-gray-800">Publicações</Link>
         <Link to="/admin/informativos" className="p-4 rounded-xl border border-gray-200 bg-white shadow-sm text-center text-xs font-semibold text-gray-800">Informativos</Link>
       </div>
     </div>
